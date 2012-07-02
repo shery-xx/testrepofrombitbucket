2b5be3faed2118828821dd57519a06260998e713
Sheraz <banckluser1@yahoo.com>
2012-07-02 22:01:12 +0500

    7th commit, add new file

---
+++ ChangeFile.cs
@@ -0,0 +1,28 @@
+﻿/*--------------------------------------------------------------------------------------+
+|   
+|  This sample is developed and maintained by CodePorting venture
+|  [http://codeporting.com ] a division of Aspose Pty Ltd.
+|
++--------------------------------------------------------------------------------------*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodePortingCSharp2Java
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    static public class SigninToken
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+
+  public static string Token = string.Empty;   
+ public int count = 200;
+public int user = 100;    
+               		
+    }
+}
