053863a26a59d3c0e331a267467222f94a5665b0
Sheraz <banckluser1@yahoo.com>
2012-07-10 21:32:55 +0500

    added Test2.cs

---
+++ Test2.cs
@@ -0,0 +1,27 @@
+﻿/*--------------------------------------------------------------------------------------+
+|   
+|  This sample is developed and maintained by CodePorting venture
+|  [http://codeporting.com ] a division of Aspose Pty Ltd.
+|
++--------------------------------------------------------------------------------------*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodePortingCSharp2Java
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    static public class SigninToken
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+  public string text= "new text here";
+  public static string Token = string.Empty;   
+  private int x = 200;
+               		
+    }
+}
